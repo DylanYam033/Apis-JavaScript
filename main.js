72f68f25e95c8947373f361ab40c3bd85ba3fe1d
Dylan <yampuezandylan04@gmail.com>
2023-01-27 10:26:37 -0500

    version 1

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-console.log('Hello, world')
-
 const container = document.querySelector('.container');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_7wVDh1Weys2nNGbMvKhtuQ7GA44iNgbyUTgLMWwwVnP1Hz7TRL2c9SK1JFvkDtdm';
+//  URL que trae las imagenes mediante metodo GET
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=1&api_key=live_7wVDh1Weys2nNGbMvKhtuQ7GA44iNgbyUTgLMWwwVnP1Hz7TRL2c9SK1JFvkDtdm';
+const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?limit=1';
 
 async function reload() {
   const res = await fetch(API_URL);
@@ -19,11 +19,26 @@ async function reload() {
 
   for(cat of data){
     const img = document.createElement('img');
+    const button = document.createElement('button');
     img.setAttribute('src', cat.url);
     img.setAttribute('width', '200');
+    button.innerText = 'Agregar a favoritos';
 
     container.appendChild(img);
+    container.appendChild(button);
+  }
+}
+
+async function loadFavoritesMichis() {
+  const res = await fetch(API_URL_FAVORITES);
+  const data = await res.json();
+  console.log('Favoritos')
+  console.log(data)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   }
 }
 
 reload();
+loadFavoritesMichis();
