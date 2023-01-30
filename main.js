3bc50a47293410520c650c9f2d563b81dcbfc7fe
Dylan <yampuezandylan04@gmail.com>
2023-01-30 11:43:50 -0500

    update 2

--- main.js
+++ main.js
@@ -1,37 +1,102 @@
-const container = document.querySelector('.container');
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=10';
+const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG';
+const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG`;
 
-//  URL que trae las imagenes mediante metodo GET
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=1';
-const API_URL_FAVORITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG';
+const spanError = document.getElementById('error')
 
-async function reload() {
-  const res = await fetch(API_URL);
+async function loadRandomMichis() {
+  const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
-
+  console.log('Random')
+  console.log(res)
   console.log(data)
 
-  for(cat of data){
-    const img = document.createElement('img');
-    const button = document.createElement('button');
-    img.setAttribute('src', cat.url);
-    img.setAttribute('width', '200');
-    button.innerText = 'Agregar a favoritos';
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status;
+  } else {
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const btn1 = document.getElementById('btn1');
+    const btn2 = document.getElementById('btn2');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
 
-    container.appendChild(img);
-    container.appendChild(button);
+    btn1.onclick = () => saveFavouriteMichi(data[0].id);
+    btn2.onclick = () => saveFavouriteMichi(data[1].id);
   }
 }
 
-async function loadFavoritesMichis() {
-  const res = await fetch(API_URL_FAVORITES);
+async function loadFavouriteMichis() {
+  const res = await fetch(API_URL_FAVOTITES);
   const data = await res.json();
   console.log('Favoritos')
   console.log(data)
 
   if (res.status !== 200) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    const section = document.getElementById('favoriteMichis')
+    section.innerHTML = "";
+
+    const h2 = document.createElement('h2');
+    const h2Text = document.createTextNode('Michis favoritos');
+    h2.appendChild(h2Text);
+    section.appendChild(h2);
+
+    data.forEach(michi => {
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const btn = document.createElement('button');
+      const btnText = document.createTextNode('Sacar al michi de favoritos');
+
+      img.src = michi.image.url;
+      img.width = 150;
+      btn.appendChild(btnText);
+      btn.onclick = () => deleteFavouriteMichi(michi.id);
+      article.appendChild(img);
+      article.appendChild(btn);
+      section.appendChild(article);
+    });
   }
 }
 
-reload();
-loadFavoritesMichis();
+async function saveFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      image_id: id
+    }),
+  });
+  const data = await res.json();
+
+  console.log('Save')
+  console.log(res)
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log('Michi guardado en favoritos')
+    loadFavouriteMichis();
+  }
+}
+
+async function deleteFavouriteMichi(id) {
+  const res = await fetch(API_URL_FAVOTITES_DELETE(id), {
+    method: 'DELETE',
+  });
+  const data = await res.json();
+
+  if (res.status !== 200) {
+    spanError.innerHTML = "Hubo un error: " + res.status + data.message;
+  } else {
+    console.log('Michi eliminado de favoritos')
+    loadFavouriteMichis();
+  }
+}
+
+loadRandomMichis();
+loadFavouriteMichis();
