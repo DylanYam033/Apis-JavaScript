253eda952ec3a5d6d5f88f461154fcdd0c03e159
Dylan <yampuezandylan04@gmail.com>
2023-01-31 16:30:32 -0500

    backend terminado

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+
+// Generar conexion a la API con axios
+// const api = axios.create({
+//   baseURL: 'https://api.thecatapi.com/v1'
+// });
+// api.defaults.headers.common['X-API-KEY'] = 'c08d415f-dea7-4a38-bb28-7b2188202e46';
+
+
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=10';
 const API_URL_FAVOTITES = 'https://api.thecatapi.com/v1/favourites?api_key=live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG';
 const API_URL_FAVOTITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}?api_key=live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG`;
+const API_URL_UPLOAD = 'https://api.thecatapi.com/v1/images/upload';
 
 const spanError = document.getElementById('error')
 
-async function loadRandomMichis() {
+async function loadRandomImages() {
   const res = await fetch(API_URL_RANDOM);
   const data = await res.json();
   console.log('Random')
@@ -22,13 +31,18 @@ async function loadRandomMichis() {
     img1.src = data[0].url;
     img2.src = data[1].url;
 
-    btn1.onclick = () => saveFavouriteMichi(data[0].id);
-    btn2.onclick = () => saveFavouriteMichi(data[1].id);
+    btn1.onclick = () => saveImages(data[0].id); //around function, cuando no queremos ejecutar una funcion automaticamente
+    btn2.onclick = () => saveImages(data[1].id);
   }
 }
 
-async function loadFavouriteMichis() {
-  const res = await fetch(API_URL_FAVOTITES);
+async function favoriteImages() {
+  const res = await fetch(API_URL_FAVOTITES,{
+    method: 'GET',
+    headers: {
+      'x-api-key': 'live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG',
+    },
+  });
   const data = await res.json();
   console.log('Favoritos')
   console.log(data)
@@ -37,7 +51,7 @@ async function loadFavouriteMichis() {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   } else {
     const section = document.getElementById('favoriteMichis')
-    section.innerHTML = "";
+    section.innerHTML = ""; //limpiar contenido cada vez que se ejecute la funcion favoriteImages()
 
     const h2 = document.createElement('h2');
     const h2Text = document.createTextNode('Michis favoritos');
@@ -53,7 +67,7 @@ async function loadFavouriteMichis() {
       img.src = michi.image.url;
       img.width = 150;
       btn.appendChild(btnText);
-      btn.onclick = () => deleteFavouriteMichi(michi.id);
+      btn.onclick = () => deleteImageSaved(michi.id);
       article.appendChild(img);
       article.appendChild(btn);
       section.appendChild(article);
@@ -61,7 +75,11 @@ async function loadFavouriteMichis() {
   }
 }
 
-async function saveFavouriteMichi(id) {
+async function saveImages(id) {
+
+  // const { data, status } = await api.post('/favourites', {
+  //   image_id: id,
+  // });
   const res = await fetch(API_URL_FAVOTITES, {
     method: 'POST',
     headers: {
@@ -80,11 +98,11 @@ async function saveFavouriteMichi(id) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   } else {
     console.log('Michi guardado en favoritos')
-    loadFavouriteMichis();
+    favoriteImages(); //ejecutamos de nuevo la funcion para evitar recargar la pagina
   }
 }
 
-async function deleteFavouriteMichi(id) {
+async function deleteImageSaved(id) {
   const res = await fetch(API_URL_FAVOTITES_DELETE(id), {
     method: 'DELETE',
   });
@@ -94,9 +112,25 @@ async function deleteFavouriteMichi(id) {
     spanError.innerHTML = "Hubo un error: " + res.status + data.message;
   } else {
     console.log('Michi eliminado de favoritos')
-    loadFavouriteMichis();
+    favoriteImages();
   }
 }
 
-loadRandomMichis();
-loadFavouriteMichis();
+async function uploadImage(){
+  const form = document.getElementById('upload')
+  const formData = new FormData(form);
+  console.log(formData.get('file'))
+
+  const res = await fetch(API_URL_UPLOAD,{
+    method: 'POST',
+    headers: { 
+      'x-api-key': 'live_3sRpuUcvQvAgSmMV1QuwuJrBdMoqzTeWvPgIW30VnVpM8X7u32blhuX9RuIo9fGG',
+    },
+    body: formData,
+  })
+
+  console.log(res);
+}
+
+loadRandomImages();
+favoriteImages();
